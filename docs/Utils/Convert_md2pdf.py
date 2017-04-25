10c6a1d13359db112b0312c92ceb9b7d3a62e13c
Comment <agovorov@luxoft.com>
2017-04-25 14:58:31 +0300

    Rebase script Changed split functionality

--- docs/Utils/Convert_md2pdf.py
+++ docs/Utils/Convert_md2pdf.py
@@ -1,31 +1,19 @@
 import os 
-import pandoc
-from markdown import markdown
-import pdfkit
 import re
 #Split all .md in one
 directory = '../Guide_index/' 
-flist = [os.path.join(pdir,f) for pdir, dirs, files in os.walk(directory) for f in files]
-for i in flist:
-	print i 
-	f1 = open("guidline.md", 'a+')
-	f2 = open( i, 'r')
-	f1_out=[]
-	f2_out=[]
-	res=[]
-	f1_out=f1.read()
-	f2_out=f2.read()
-	res=f1_out+f2_out
-	f1.write(res)
-	f1.close()
-	f2.close()
-#Rebase link
-md = open("guidline.md", 'r+')
+filelist = sorted([os.path.join(pdir, f) for pdir, dirs, files in os.walk(directory) for f in files])
+for i in filelist:
+    print i
+    f = open('guidline.md', 'a')
+    f.write((open(i, 'r')).read())
+    f.close()
+#Conver link
+temp = open("guidline.md", 'r+')
 for line in md:
     line = re.sub(r'(\.\./+.+\.md)','', line)
-    print line 
-    f=open("guidlineresult.md", 'a')
+    f = open("guidlineresult.md", 'a')
     f.write(line)
-       
 f.close()    
-md.close()
+temp.close()
+
